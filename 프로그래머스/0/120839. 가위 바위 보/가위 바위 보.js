323583fe605fa10e66664495fd3ceb6ce8644308
전선우 <163851753+xx-lvr@users.noreply.github.com>
2024-09-16 21:45:05 +0900

    [level 0] Title: 가위 바위 보, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.js
@@ -0,0 +1,14 @@
+function solution(rsp) {
+    var result = "";
+    for (let i = 0; i < rsp.length; i++){
+        if(rsp[i] === "2"){
+            result += "0";
+        }
+        else if(rsp[i] === "0"){
+            result += "5";
+        }
+        else
+            result += "2";
+    }
+    return result;
+}
