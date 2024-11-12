eb0dc629ba495ebd99094ff14558f646e1ad2b73
전선우 <163851753+xx-lvr@users.noreply.github.com>
2024-11-12 22:51:33 +0900

    [Bronze V] Title: A+B - 3, Time: 0 ms, Memory: 1124 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+        int a, b, c;
+        scanf("%d", &c);
+        for (int i = 0; i < c; i++){
+        scanf("%d %d", &a, &b);
+        printf("%d\n", a + b);
+    }
+    return 0;
+}
