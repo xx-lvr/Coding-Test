a24df478b010605f2ef41d461188770076527864
전선우 <163851753+xx-lvr@users.noreply.github.com>
2024-12-10 18:04:56 +0900

    Update 팩토리얼 0의 개수.kt

--- 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.kt
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.kt
@@ -9,3 +9,10 @@ fun main() {
     }
     println(cnt)
 }
+
+/* val n = readLine()!!.toInt(): 입력을 받은 n을 정수로 변환
+var cnt = 0: 뒤에 나오는 0의 개수를 셈.
+var div = 5: 5의 배수를 확인하는 변수.
+while (n / div > 0): 5, 25, 125 등의 배수를 하나씩 확인.
+cnt += n / div: 각 배수에서 나오는 0의 개수를 더함.
+div *= 5: 5의 배수를 점차 증가시키며 확인. */
