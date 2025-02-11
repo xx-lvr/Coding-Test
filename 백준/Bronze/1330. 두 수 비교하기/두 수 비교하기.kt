c80b2772ce467e278e086bbf4b985192d5dca556
전선우 <163851753+xx-lvr@users.noreply.github.com>
2025-02-11 21:31:01 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 120 ms, Memory: 20860 KB -BaekjoonHub

--- 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.kt
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.kt
@@ -1,6 +1,8 @@
 fun main() {
     val (a, b) = readLine()!!.split(" ").map {it.toInt()}
-    if(a > b) println(">")
-    if(a < b) println("<")
-    if(a == b) println("==")
+    when {
+        a > b -> println(">")
+        a < b -> println("<")
+        else -> println("==")
+    }
 }
