a339fedfb38cfdf402d70dfde34614c2513aece8
전선우 <163851753+xx-lvr@users.noreply.github.com>
2026-03-26 10:47:48 +0900

    [Silver V] Title: 나이순 정렬, Time: 2768 ms, Memory: 46020 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -0,0 +1,11 @@
+n = int(input())
+arr = []
+
+for _ in range(n):
+    age, name = input().split()
+    arr.append((int(age), name))
+
+arr.sort(key=lambda x: x[0])
+
+for age, name in arr:
+    print(age, name)
