210d5406a055cde8b171f5ff34dc0dd4d6735cd6
전선우 <163851753+xx-lvr@users.noreply.github.com>
2026-04-03 10:08:19 +0900

    [Gold V] Title: 적록색약, Time: 68 ms, Memory: 34992 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -7,34 +7,34 @@ matrix = [list(input().strip()) for _ in range(n)]
 visited = [[False] * n for _ in range(n)]
 dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 
-def bfs(x, y, visited, board):
+def bfs(x, y, visited):
     q = deque([(x, y)])
     visited[x][y] = True
-    color = board[x][y]
+    color = matrix[x][y]
 
     while q:
         ax, ay = q.popleft()
         for bx, by in dirs:
             nx, ny = ax + bx, ay + by
-            if 0 <= nx < n and 0 <= ny < n and not visited[nx][ny] and board[nx][ny] == color:
+            if 0 <= nx < n and 0 <= ny < n and not visited[nx][ny] and matrix[nx][ny] == color:
                 visited[nx][ny] = True
                 q.append((nx, ny))
     
-def count_regions(visited, board):
+def count_regions(visited):
     count = 0
     for i in range(n):
         for j in range(n):
             if not visited[i][j]:
-                bfs(i, j, visited, board)
+                bfs(i, j, visited)
                 count += 1
     return count
 
-normal_count = count_regions(visited, matrix)
+normal_count = count_regions(visited)
 for i in range(n):
     for j in range(n):
         if matrix[i][j] == 'G':
             matrix[i][j] = 'R'
 
 visited = [[False] * n for _ in range(n)]
-color_blind_count = count_regions(visited, matrix)
+color_blind_count = count_regions(visited)
 print(normal_count, color_blind_count)
