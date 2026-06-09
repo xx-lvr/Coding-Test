e935a45005dadc33b0884a1066a700ef1dc312d4
전선우 <163851753+xx-lvr@users.noreply.github.com>
2026-06-09 13:39:44 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,11 +1,8 @@
 def solution(array, commands):
     answer = []
     for command in commands:
-        i = command[0]
-        j = command[1]
-        k = command[2]
-        new_array = array[i - 1 : j]
+        new_array = array[command[0] - 1 : command[1]]
         new_array.sort()
-        k_num = new_array[k - 1]
+        k_num = new_array[command[2] - 1]
         answer.append(k_num)
     return answer
