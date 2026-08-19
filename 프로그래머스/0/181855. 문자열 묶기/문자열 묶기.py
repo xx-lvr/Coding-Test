005387d558f131329841ba79c5e1dfc314ee9552
전선우 <163851753+xx-lvr@users.noreply.github.com>
2026-08-20 00:40:41 +0900

    [level 0] Title: 문자열 묶기, Time: 6.98 ms, Memory: 17.3 MB -BaekjoonHub

--- 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.py
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.py
@@ -1,11 +1,17 @@
 def solution(strArr):
     answer = 0
     count = {}
+
     for i in strArr:
         length = len(i)
+
         if length in count:
             count[length] = count[length] + 1
         else:
             count[length] = 1
-    answer = max(count.values())
+
+    for i in count:
+        if count[i] > answer:
+            answer = count[i]
+
     return answer
