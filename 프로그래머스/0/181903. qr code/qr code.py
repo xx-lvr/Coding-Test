0330cd14c3782bd3bfe0b6ae5c781c359e957c60
전선우 <163851753+xx-lvr@users.noreply.github.com>
2026-08-22 15:50:32 +0900

    [level 0] Title: qr code, Time: 0.06 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/0/181903. qr code/qr code.py
+++ 프로그래머스/0/181903. qr code/qr code.py
@@ -2,5 +2,5 @@ def solution(q, r, code):
     answer = ''
     for i in range(len(code)):
         if i % q == r:
-            answer += str(code[i])
+            answer += code[i]
     return answer
