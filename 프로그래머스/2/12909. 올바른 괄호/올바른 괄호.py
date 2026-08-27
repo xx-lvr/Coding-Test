ebfd8674503e9cea52d075f1f13118dc0b618535
전선우 <163851753+xx-lvr@users.noreply.github.com>
2026-08-28 08:50:30 +0900

    [level 2] Title: 올바른 괄호, Time: 5.20 ms, Memory: 11.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,14 @@
+def solution(s):
+    count = 0
+    for i in s:
+        if i == "(":
+            count += 1
+        else:
+            count -= 1
+        if count < 0:
+            return False
+    
+    if count == 0:
+        return True
+    else:
+        return False
